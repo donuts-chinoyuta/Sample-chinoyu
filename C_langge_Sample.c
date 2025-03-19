56046db50eba2a0d6e23d6a453e707e92d503f1c
donuts-chinoyuta <chino.yuta@donuts.ne.jp>
2025-03-19 17:25:04 +0900

    標準偏差の計算を追加

--- C_langge_Sample.c
+++ C_langge_Sample.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     int n, i;
-    float sum = 0.0, average, SD;
+    float sum = 0.0, average, SD, AD ;
+    float total = 0;
+    float InSD;
 
     printf("データの数を入力してください: ");
     scanf("%d", &n);
 
     float numbers[n]; // 配列を宣言
+    float Snumber[n];
 
     // 数値を入力
     for (i = 0; i < n; i++) {
@@ -19,13 +23,18 @@ int main() {
 
     average = sum / n; // 平均を計算
 
+    for(i =0; i < n; i++){
+        SD = numbers[i] - average;
+        Snumber[i] = SD*SD;
+        total += Snumber[i];
+    }
 
-
+    InSD = sqrt(total);
+    
 
     printf("平均値は: %.2f\n", average);
     printf("testtest\n");
-
-    
+    printf("標準偏差は: %.2f\n", InSD);
 
     return 0;
 
